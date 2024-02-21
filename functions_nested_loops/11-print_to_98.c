325f16d5209e3905f9d4779c3016232bc179d70a
SamuelS53 <samuel.serrand@laposte.net>
2024-02-21 19:18:10 +0000

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 /**
-*add - sums up two numbers
-*@n:first integer
-*@m:second integer
-*Return: returns 0
+*print_to_98 - prints natural
+*numbers from n to 98
+*@n:integer to start by
+*return: returns nothing
 */
-int add(int n, int m)
+void print_to_98(int n)
 {
-return (n + m);
+if (n >= 98)
+{
+while (n > 98)
+printf("%d, ", n--);
+
+printf("%d\n", n);
+}
+else
+{
+while (n < 98)
+printf("%d, ", n++);
+
+printf("%d\n", n);
+}
 }
